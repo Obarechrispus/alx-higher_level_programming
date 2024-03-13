01ba676e49df212339b20acbe200fffce11772de
Obarechrispus <obarechrispus@gmail.com>
2024-03-13 03:21:11 -0700

    task3

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-print(''.join('{}' for _ in range(ord('a'), ord('z') + 1)).format(*(chr(i) for i in range(ord('a'), ord('z') + 1))))
+for i in range (ord('a'), ord('z') + 1):
+    print (chr(i), end='')
 
