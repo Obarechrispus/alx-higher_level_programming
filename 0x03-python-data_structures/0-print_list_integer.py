7e1e4547b593e860e42fa5d0fcbc78049ebf035e
Obarechrispus <obarechrispus@gmail.com>
2024-03-18 12:30:43 -0700

    task1

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bash/python3
-#if __name__ == "__main__"
-def print_list_integer(my_list=[]):
-    for num in my_list:
-        print("{:}".format(num))
+if __name__ == "__main__":
+    def print_list_integer(my_list=[]):
+        for num in my_list:
+            print("{:}".format(num))
---
+++ 0x03-python-data_structures/lists.h
@@ -0,0 +1,5 @@
+#ifndef LIST_H
+#define LIST_H
+def element_at(my_list, idx);
+def print_list_integer(my_list=[]);
+#endif
