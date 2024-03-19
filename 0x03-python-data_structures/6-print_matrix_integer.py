d1e779a452d4109fb5bb8031da18f1e98707e2c8
Obarechrispus <obarechrispus@gmail.com>
2024-03-19 03:14:05 -0700

    task6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bash/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for element in row:
+            print(element, end=" ")
+        print()
+
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -5,4 +5,11 @@ def element_at(my_list, idx);
 def replace_in_list(my_list, idx, element);
 def print_reversed_list_integer(my_list=[]);
 def new_in_list(my_list, idx, element);
+int is_palindrome(listint_t **head);
+def delete_at(my_list=[], idx=0);
+def divisible_by_2(my_list=[]);
+def max_integer(my_list=[]);
+def multiple_returns(sentence);
+def add_tuple(tuple_a=(), tuple_b=());
+def print_matrix_integer(matrix=[[]]);
 #endif
