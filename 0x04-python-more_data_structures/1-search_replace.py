7a244f42d23e9d1e364570f14c6ea185615c9167
Obarechrispus <obarechrispus@gmail.com>
2024-03-20 06:36:09 -0700

    task1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new = my_list.copy()
-    for i in range(len(new)):
-        if new[i] == search:
-            new[i] = replace
+    new_list = my_list.copy()
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
         
-    return new
+    return new_list
     return my_list
 
