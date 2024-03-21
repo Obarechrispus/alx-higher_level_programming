f3b8056ba14a98386c48302b4c0444dca6c4e850
Obarechrispus <obarechrispus@gmail.com>
2024-03-21 13:15:04 -0700

    task0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,11 @@
 #include "lists.h"
+/**
+ *print_dlistint - a function that prints the number of elements in a linked list
+ *@h: number of elements
+ *
+ *
+ *Returnn: node
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t node = 0;
