220cf2ade78fad4584c3e29c62d200f0fc07e765
Obarechrispus <obarechrispus@gmail.com>
2024-04-10 01:14:29 -0700

    task1

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 class Rectangle:
-    """
-    def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
-        print(type(Rectangle))
-    """
     pass
