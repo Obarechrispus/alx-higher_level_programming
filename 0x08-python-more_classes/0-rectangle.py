6d45c2f3a97d9362c7fde54147bacd5d0b01b35b
Obarechrispus <obarechrispus@gmail.com>
2024-04-10 05:58:31 -0700

    task1

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+
 """ 
 the doc string to the  Recatngle module
 """
+
 class Rectangle:
+
     """
     this is the class to the Rectangle module
-    """ 
+    """
+
     pass
