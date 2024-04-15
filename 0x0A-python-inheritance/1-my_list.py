e2023e80b721f88ad9167d7ee9bb5bd3b8412821
Obarechrispus <obarechrispus@gmail.com>
2024-04-15 11:21:28 -0700

    update

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -12,6 +12,5 @@ class MyList(list):
         """
         Prints the list in sorted order (ascending).
         """
-        sorted_list = sorted(self)
-        print(sorted_list)
+        print(sorted(self))
 
