eff3c3fda0f6661e5b8115ae73181564d0991d4f
Obarechrispus <obarechrispus@gmail.com>
2024-04-15 10:40:34 -0700

    task1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+class MyList(list):
+    """
+    A custom list class that inherits from the built-in list class.
+    """
+
+    def print_sorted(self):
+        """
+        Prints the list in sorted order (ascending).
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
+
