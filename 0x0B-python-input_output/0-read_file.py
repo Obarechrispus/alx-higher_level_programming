6b1ab552266fe4a0f12c2711760269f3dd02cf46
Obarechrispus <obarechrispus@gmail.com>
2024-04-17 05:04:03 -0700

    task`1

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+ It reads the file line by line and prints each line to stdout.
+"""
+
+
+def read_file(filename=""):
+    """This function, read_file, takes an optional parameter filename which defaults to an empty string."""
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            print(line, end='')
