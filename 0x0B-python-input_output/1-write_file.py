4b06b9650e53fd30b945fec481441d7aec88c35a
Obarechrispus <obarechrispus@gmail.com>
2024-04-18 08:09:16 -0700

    task2

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
 A function that takes two parameters filename and text
-filename and teext are defined without actual value 
+filename and teext are defined without actual value
 and when we call the calue they will adopt new values else considered empty
 """
 
 
 def write_file(filename="", text=""):
     """
-    we W+ to writte into a file 
-    we use the UTF- to make sure the string we are geting is properly converted into binary
+    we W+ to writte into a file
+    we use the UTF- to string we are geting is properly converted into binary
     the command file.write calls the recently opened file for writting
     """
     with open(filename, 'w+', encoding='utf-8') as file:
