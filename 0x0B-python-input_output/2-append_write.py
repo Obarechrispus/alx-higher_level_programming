79f1d5e5c5649efabc0cc825f62b8375049e843f
Obarechrispus <obarechrispus@gmail.com>
2024-04-18 23:44:53 -0700

    task1

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""
+a module thaat appends to the end of the file
+"""
 
 
 def append_write(filename="", text=""):
+    """
+    takes two parameter filename and text,
+    """
 
     with open(filename, 'a', encoding='utf-8') as a file:
         start = file.tell()
