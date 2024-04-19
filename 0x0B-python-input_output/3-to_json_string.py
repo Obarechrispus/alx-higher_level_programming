9580887337991e1c46db8b81906a57d995a32413
Obarechrispus <obarechrispus@gmail.com>
2024-04-19 00:22:04 -0700

    task1

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/python3
+"""
+a function that return the json value of an object
+"""
+
+import json
+def to_json_string(my_obj):
+    """ 
+    takes only one parameter 
+    we had to import from json
+    """
+    return json.dumps(my_obj)
+
