e22e1b1b2891f7bd45d7a60fe861c4d0635c3c8e
Obarechrispus <obarechrispus@gmail.com>
2024-04-19 00:53:38 -0700

    update

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 import json
 
+
 """
 a function that return the python data structure as represented by json string
 """
+
+
 def from_json_string(my_str):
     """
     takes a single parameter
