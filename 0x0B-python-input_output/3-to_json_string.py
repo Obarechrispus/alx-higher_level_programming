097e21ab8a39784eb375704590473b827aa3e55b
Obarechrispus <obarechrispus@gmail.com>
2024-04-19 00:35:00 -0700

    ls

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -3,11 +3,11 @@
 a function that return the json value of an object
 """
 
+
 import json
 def to_json_string(my_obj):
-    """ 
-    takes only one parameter 
+    """
+    takes only one parameter
     we had to import from json
     """
     return json.dumps(my_obj)
-
