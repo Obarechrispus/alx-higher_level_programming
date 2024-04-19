bea9ec735f4da53e99ea65dc969aaf757457f050
Obarechrispus <obarechrispus@gmail.com>
2024-04-19 02:05:15 -0700

    update

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -3,6 +3,7 @@
 A function that writes into a file using the json interpretation
 """
 
+
 import json
 def save_to_json_file(my_obj, filename):
     """
@@ -10,5 +11,5 @@ def save_to_json_file(my_obj, filename):
     the name of the file is filename
     """
     json_str = json.dumps(my_obj)
-    with open(filename, 'w+', encoding='utf-8') as file:
+    with open(filename, 'w', encoding='utf-8') as file:
         file.write(json_str)
