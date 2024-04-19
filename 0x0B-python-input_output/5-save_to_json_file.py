6ebb43d094c1f5175baada833f341a00fc1e55ad
Obarechrispus <obarechrispus@gmail.com>
2024-04-19 02:02:12 -0700

    update

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+A function that writes into a file using the json interpretation
+"""
+
+import json
+def save_to_json_file(my_obj, filename):
+    """
+    the objest contains the json scripts while
+    the name of the file is filename
+    """
+    json_str = json.dumps(my_obj)
+    with open(filename, 'w+', encoding='utf-8') as file:
+        file.write(json_str)
